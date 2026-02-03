1a4023372069cc464b5afa180488919e534544ee
exliang <exliang@uci.edu>
2026-02-02 18:41:15 -0800

    add test url

--- crawler/worker.py
+++ crawler/worker.py
@@ -18,6 +18,7 @@ class Worker(Thread):
         super().__init__(daemon=True)
         
     def run(self):
+        print("run called")
         while True:
             tbd_url = self.frontier.get_tbd_url()
             if not tbd_url:
@@ -28,7 +29,7 @@ class Worker(Thread):
                 f"Downloaded {tbd_url}, status <{resp.status}>, "
                 f"using cache {self.config.cache_server}.")
             scraped_urls = scraper.scraper(tbd_url, resp)
-            print("scraper called")
+            print("scraper called") #TEST
             for scraped_url in scraped_urls:
                 self.frontier.add_url(scraped_url)
             self.frontier.mark_url_complete(tbd_url)
